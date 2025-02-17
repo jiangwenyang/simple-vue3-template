524ed47b34408fdff2efdbcda76e3fea39f6dbf6
jiangwenyang <wenyang.jay@gmail.com>
2025-02-17 17:11:05 +0800

    feat: 修改README

--- src/App.vue
+++ src/App.vue
@@ -7,5 +7,5 @@
 <script lang="ts" setup>
 import AppProvider from './AppProvider.vue'
 import APPView from './APPView.vue';
-document.title = import.meta.env.VITE_APP_TITLE || '一路三方'
+document.title = import.meta.env.VITE_APP_TITLE || '项目名称'
 </script>
